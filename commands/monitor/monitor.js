a4f250aa1b23cb9a60d4bf3056925497273ca0ae
Mythofs <bxawesome1@gmail.com>
2026-03-11 15:33:56 -0400

    Deleted currentListing and changed monitor.js to use api v1

--- commands/monitor/monitor.js
+++ commands/monitor/monitor.js
@@ -3,7 +3,6 @@ const { apiKey,ffscouterKey,channelId } = require('../../config.json');
 const monitor = require('../../monitorStore.js');
 const idCache = require('../../idCache.js');
 const bstotal = require('../../bstotal.js');
-const currentListings = require('../../currentListings.js');
 
 module.exports = {
     data: new SlashCommandBuilder().setName('monitor').setDescription('Monitor an item.')
@@ -33,26 +32,17 @@ module.exports = {
                             if(idCache.get(key).size == 0)
                                 idCache.delete(key);
                         }
-                    for(const listing of currentListings) {
-
-                    }
                     if(profileId.size == 0)
                         return;
                     const stats = await safeFetch(`https://ffscouter.com/api/v1/get-stats?key=${ffscouterKey}&targets=${Array.from(profileId).join()}`, channel);
                     for(const stat of stats) {
                         if(stat.bs_estimate < bstotal * 0.75) {
-                            const icons = await safeFetch(`https://api.torn.com/v2/user/${stat.player_id}/icons?comment=Weav3r%20Mug%20Bot&key=${apiKey}`, channel);
-                            const iconMap = new Map(icons.icons.map(icon => [icon.id, icon.description]));
+                            const userInfo = await safeFetch(`https://api.torn.com/user/${stat.player_id}?selections=icons,bazaar,profile&key=${apiKey}`, channel);
+                            const iconMap = new Map(userInfo.icons.map(icon => [icon.id, icon.description]));
                             //72 - greenleaf, 71 - traveling, 15 - hostpital, 35 - bazaar, 27 - company
-                            if(!iconMap.has(72) && iconMap.has(71) && !iconMap.has(15) && (!iconMap.has(27) || !iconMap.get(27).includes("Clothing Store")))
-                                if(currentListings.has(stat.player_id))
-                                    currentListings.get(stat.player_id).add({itemId: itemId, bs: stat.bs_estimate_human, value: currentListing.price * currentListing.quantity, timestamp: Date.now()});
-                                else
-                                    currentListings.set(stat.player_id, {itemId: itemId, bs: stat.bs_estimate_human, value: currentListing.price * currentListing.quantity, timestamp: Date.now()});
                             if(!iconMap.has(72) && !iconMap.has(71) && !iconMap.has(15) && iconMap.has(35) && (!iconMap.has(27) || !iconMap.get(27).includes("Clothing Store")) && !(idCache.has(stat.player_id) && idCache.get(stat.player_id).has(itemId))) {
-                                const currentListing = listings.find(l => l.player_id == stat.player_id);
-                                const profile = await safeFetch(`https://api.torn.com/v2/user/${stat.player_id}/profile?striptags=true&comment=Weav3r%20Mug%20Bot&key=${apiKey}`, channel);
-                                if(profile.profile.last_action.status != "Online") {
+                                const listing = listings.find(l => l.player_id == stat.player_id);
+                                if(userInfo.last_action.status != "Online") {
                                     channel.send({
                                         content: '@everyone',
                                         allowedMentions: { parse: ['everyone'] },
@@ -62,22 +52,22 @@ module.exports = {
                                         .setColor(0xF59E0B)
                                         .addFields(
                                             { name: 'Item', value: data.item_name, inline: true },
-                                            { name: 'Price', value: '$' + currentListing.price.toLocaleString(), inline: true },
-                                            { name: 'Total Value', value: '$' + (currentListing.price * currentListing.quantity).toLocaleString(), inline: true },
-                                            { name: 'Name', value: `${profile.profile.name} (${profile.profile.id})`, inline: true},
-                                            { name: 'Level', value: String(profile.profile.level), inline: true},
-                                            { name: 'Last Action', value: profile.profile.last_action.status + " (" + profile.profile.last_action.relative + ")", inline: true},
+                                            { name: 'Price', value: '$' + listing.price.toLocaleString(), inline: true },
+                                            { name: 'Total Value', value: '$' + (listing.price * listing.quantity).toLocaleString(), inline: true },
+                                            { name: 'Name', value: `${userInfo.name} (${userInfo.id})`, inline: true},
+                                            { name: 'Level', value: String(userInfo.level), inline: true},
+                                            { name: 'Last Action', value: userInfo.last_action.status + " (" + userInfo.last_action.relative + ")", inline: true},
                                             { name: 'Estimated Stats', value: stat.bs_estimate_human ?? 'Unknown', inline: true },
-                                            { name: 'Bazaar Link', value: `[Bazaar](https://www.torn.com/bazaar.php?userId=${profile.profile.id}#/)` },
-                                            { name: 'Profile Link', value: `[Profile](https://www.torn.com/profiles.php?XID=${profile.profile.id})` },
-                                            { name: 'Attack Link', value: `[Attack](https://www.torn.com/loader.php?sid=attack&user2ID=${profile.profile.id})` },
+                                            { name: 'Bazaar Link', value: `[Bazaar](https://www.torn.com/bazaar.php?userId=${userInfo.id}#/)` },
+                                            { name: 'Profile Link', value: `[Profile](https://www.torn.com/profiles.php?XID=${userInfo.id})` },
+                                            { name: 'Attack Link', value: `[Attack](https://www.torn.com/loader.php?sid=attack&user2ID=${userInfo.id})` },
                                         )
                                         .setTimestamp()
                                     ]});
-                                    if(idCache.has(profile.profile.id))
-                                        idCache.get(profile.profile.id).add(itemId);
+                                    if(idCache.has(userInfo.id))
+                                        idCache.get(userInfo.id).add(itemId);
                                     else
-                                        idCache.set(profile.profile.id, new Set([itemId]));
+                                        idCache.set(userInfo.id, new Set([itemId]));
                                 }
                             }
                         }
